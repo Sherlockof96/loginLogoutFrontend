7bd9dd30aad43a67c2df65eb7856798aa9ea8a25
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-22 16:34:44 -0700

    cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
+import Cookie from 'js-cookie'
 
 export function middleware(req: NextRequest, res: NextResponse) {
     console.log('middleware')
@@ -9,11 +10,11 @@ export function middleware(req: NextRequest, res: NextResponse) {
 
     if (req.cookies.has('x-redirect-url')) {
         console.log(req.cookies.get('x-redirect-url').value)
-
+        Cookie.set('x-redirect-url', req.cookies.get('x-redirect-url').value)
         response.cookies.set('x-go-to', req.cookies.get('x-redirect-url').value)
     }
     else {
-        
+        Cookie.set('x-redirect-url', "https://kavishdoshi.com")
         response.cookies.set('x-go-to', "https://kavishdoshi.com")
     }
     
